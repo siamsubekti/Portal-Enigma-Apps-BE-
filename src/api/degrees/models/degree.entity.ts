25b4cb2387df276c7ab98ce26475699b4d88d805
Reza Fajriansyah <reza.fajriansyah@sqrtechno.com>
2019-07-12 11:16:56 +0700

    add entity and module degree

---
+++ src/api/degrees/models/degree.entity.ts
@@ -0,0 +1,14 @@
+import { PrimaryColumn, Generated, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('mst_degrees')
+export default class Degree {
+    @PrimaryColumn({ type: 'int', unsigned: true})
+    @Generated()
+    id: number;
+
+    @Column({ type: 'varchar', length: 10, nullable: false})
+    name: string;
+
+    @Column({ type: 'timestamp', nullable: false, default: () => 'CURRENT_TIMESTAMP'})
+    createdAt: Date;
+}
