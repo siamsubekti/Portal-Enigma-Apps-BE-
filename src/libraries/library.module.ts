55ce0731df0811c504e7f6ee8e55480fdd91583e
Reza Fajriansyah <reza.fajriansyah@sqrtechno.com>
2019-07-12 11:21:31 +0700

    add role entity and change config

--- src/libraries/library.module.ts
+++ src/libraries/library.module.ts
@@ -1,11 +1,11 @@
 import { Module, Global } from '@nestjs/common';
-import { AppConfig } from '../config/app.config';
 import { HashUtil } from './utilities/hash.util';
 import ResponseUtil from './response/response.util';
+import { ConfigModule } from '../config/config.module';
 
 @Global()
 @Module({
-  imports: [AppConfig],
+  imports: [ConfigModule],
   exports: [HashUtil, ResponseUtil],
   providers: [HashUtil, ResponseUtil],
 })
