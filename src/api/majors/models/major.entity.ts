0edbb22edf0dd3737bf4918b1fc7dc678d9f875a
Reza Fajriansyah <reza.fajriansyah@sqrtechno.com>
2019-07-12 11:17:57 +0700

    add entity and dto major

---
+++ src/api/majors/models/major.entity.ts
@@ -0,0 +1,14 @@
+import { PrimaryColumn, Generated, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('mst_majors')
+export default class Major {
+    @PrimaryColumn({ type: 'int', unsigned: true})
+    @Generated()
+    id: number;
+
+    @Column({ type: 'varchar', length: 255, nullable: false})
+    name: string;
+
+    @Column({ name: 'created_at', type: 'timestamp', nullable: false, default: () => 'CURRENT_TIMESTAMP'})
+    createdAt: Date;
+}
