0bda2a30271a854454b0dd6334332950f89ce97d
Reza Fajriansyah <reza.fajriansyah@sqrtechno.com>
2019-08-01 11:43:39 +0700

    updated import module

--- src/api/accounts/account.module.ts
+++ src/api/accounts/account.module.ts
@@ -6,7 +6,7 @@ import AccountService from './services/account.service';
 import ProfileService from './services/profile.service';
 import AccountController from './controllers/account.controller';
 import { RoleModule } from '../master/roles/role.module';
-import { MenuModule } from '../master/menus/menu.module';
+import MenuModule from '../master/menus/menu.module';
 import ServicesModule from '../master/services/services.module';
 
 @Module({
