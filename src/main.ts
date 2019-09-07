2ac9d7479971ec7d8b788539aff494b09fa7a843
adisthya <adisthya.rahmadyan@gmail.com>
2019-09-07 13:29:53 +0700

    Updated CORS config.

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap(): Promise<void> {
 
   generateSwagger(app);
 
-  app.enableCors();
+  app.enableCors({ origin: process.env.API_DOMAIN, credentials: true });
   app.use(compression());
   app.use(helmet());
   app.use(cookieParser());
